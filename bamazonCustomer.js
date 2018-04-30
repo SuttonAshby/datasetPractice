41e4e30301768cbd4024e44c45417672e9cc6564
Sutton Ashby <korim_fidelis@yahoo.com>
2018-04-30 18:25:52 -0400

    creates data display now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,6 +7,30 @@ var connection = mysql.createConnection({
     user: "root",
     password: "b4ca81n!",
     database: "bamazon"
-  });
+});
 
-  
+var app = {
+    initialize: function () {
+        app.loadData();
+    },
+    loadData: function () {
+        connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+            if (err) throw err;
+            // console.log(res);
+            console.log("ID || Product || Price ($) ||")
+            for (var i = 0; i < res.length; i++) {
+                var current = "";
+                for (key in res[i]) {
+                    current += res[i][key];
+                    current += " || ";
+                }
+                console.log("=============================")
+                console.log(current)
+            }
+            console.log("=============================")
+
+        });
+    }
+}
+
+app.initialize()
