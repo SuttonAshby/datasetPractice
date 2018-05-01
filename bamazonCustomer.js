16654a0bb89cd938ec26b463f1a2eb3438d6ba8e
Sutton Ashby <korim_fidelis@yahoo.com>
2018-05-01 01:46:38 -0400

    initial questions added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,9 +28,41 @@ var app = {
                 console.log(current)
             }
             console.log("=============================")
+            app.ask()
 
         });
+    },
+    ask: function(){
+        inquirer.prompt([{
+            type: "input",
+            message: "What is the ID of the item you wish to purchase?",
+            name: "item"
+        },{
+            type: "input",
+            message: "What quantity do you wish to purchase?",
+            name: "quantity"
+        }]).then(function(inquirerResponse){
+            connection.query("SELECT stock_quantity FROM products WHERE ?",{item_id: inquirerResponse.item}, function (err, res) {
+                if (err) throw err;
+                // console.log(res);
+                if(inquirerResponse.quantity > res[0].stock_quantity){
+                    console.log("Sorry there is insufficent supply in stock")
+                    console.log("You will now be redirected to the home page")
+                    app.initialize()
+                } else {
+                    var updatedQuantity = parseInt(res[0].stock_quantity - inquirerResponse.quantity)
+                    connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity: updatedQuantity},{item_id: inquirerResponse.item}], function (err, res) {
+                        if (err) throw err;
+                        // console.log(res);
+                        console.log()
+            
+                    });
+                }
+            });
+
+        })
     }
+
 }
 
 app.initialize()
